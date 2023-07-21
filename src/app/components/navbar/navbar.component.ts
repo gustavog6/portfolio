c8728d51b3decf15577b7d3998293109af136641
gustavog6 <gustavoadolfogg@gmail.com>
2023-07-21 00:36:01 -0400

    navbar fixed

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -11,9 +11,9 @@ type tabType = 'home' | 'about-me' | 'skills' | 'portfolio' | 'contact';
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
-
   public currentTab: tabType = 'home';
 
+  public aria: true | false = false;
 
   rrssLinks: RRSS[];
 
@@ -21,30 +21,14 @@ export class NavbarComponent {
     this.rrssLinks = link.rrss;
   }
 
-  // set the target element that will be collapsed or expanded (eg. navbar menu)
-  $targetEl = document.getElementById('targetEl');
-
-  // optionally set a trigger element (eg. a button, hamburger icon)
-  $triggerEl = document.getElementById('triggerEl');
-
-  // optional options with default values and callback functions
-  options = {
-    onCollapse: () => {
-      console.log('element has been collapsed');
-    },
-    onExpand: () => {
-      console.log('element has been expanded');
-    },
-    onToggle: () => {
-      console.log('element has been toggled');
-    },
-  };
-
-  collapse = new Collapse(this.$targetEl, this.$triggerEl, this.options);
-
   getLink() {
     return this.rrssLinks
       .filter((item) => item.title === 'Whatsapp')
       .map((link) => link.link);
+  }
+
+  clicked(ct: tabType) {
+    this.currentTab = ct;
+    this.aria = false;
   }
 }
